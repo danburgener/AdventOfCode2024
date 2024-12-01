62d1f6d6d5281fff55ebf604d06c0a2396e97dcb
Dan Burgener <dan.burgener@gmail.com>
2024-11-30 23:28:11 -0700

    Add Day 2 placeholders

---
+++ Day02.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AdventOfCode2024
+{
+    public static class Day02
+    {
+        public static async Task<long> One()
+        {
+            var data = await Common.ReadFile("Two", "One");
+            return 0;
+        }
+
+        public static async Task<long> Two()
+        {
+            var data = await Common.ReadFile("Two", "Two");
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine("Day 1");
 Console.WriteLine($"{nameof(Day01.One)}: {await Day01.One()}");
 Console.WriteLine($"{nameof(Day01.Two)}: {await Day01.Two()}");
+
+Console.WriteLine("Day 2");
+Console.WriteLine($"{nameof(Day02.One)}: {await Day02.One()}");
+Console.WriteLine($"{nameof(Day02.Two)}: {await Day02.Two()}");
