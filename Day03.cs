c32a46f9f394fce039625dbebacdac68860b8904
Dan Burgener <dan.burgener@gmail.com>
2024-12-03 08:37:04 -0700

    Day 3 complete

---
+++ Day03.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024
+{
+    public static class Day03
+    {
+        public static async Task<long> One()
+        {
+            var data = await Common.ReadFile("Three", "One");
+            string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+            var regex = new System.Text.RegularExpressions.Regex(pattern);
+            MatchCollection matches = regex.Matches(string.Join(' ',data));
+            int total = 0;
+            foreach ( Match match in matches ) {
+                total += (int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+            }
+
+            return total;
+        }
+
+        public static async Task<long> Two()
+        {
+            var data = await Common.ReadFile("Three", "Two");
+            string pattern = @"(?:mul\((\d{1,3}),(\d{1,3})\))|(?:do\(\))|(?:don\'t\(\))";
+            var regex = new System.Text.RegularExpressions.Regex(pattern);
+            MatchCollection matches = regex.Matches(string.Join(' ', data));
+            int total = 0;
+            bool enabled = true;
+            foreach (Match match in matches)
+            {
+                if (match.Value == "don't()")
+                {
+                    enabled = false;
+                }
+                else if (match.Value == "do()")
+                {
+                    enabled = true;
+                }
+                else
+                {
+                    if (enabled)
+                    {
+                        total += (int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+                    }
+                }
+            }
+
+            return total;
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,3 +7,7 @@ Console.WriteLine($"{nameof(Day01.Two)}: {await Day01.Two()}");
 Console.WriteLine("Day 2");
 Console.WriteLine($"{nameof(Day02.One)}: {await Day02.One()}");
 Console.WriteLine($"{nameof(Day02.Two)}: {await Day02.Two()}");
+
+Console.WriteLine("Day 3");
+Console.WriteLine($"{nameof(Day03.One)}: {await Day03.One()}");
+Console.WriteLine($"{nameof(Day03.Two)}: {await Day03.Two()}");
