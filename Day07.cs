279ea4c6eb11d0506802bbe0bacca0e3dd276b3f
Dan Burgener <dan.burgener@gmail.com>
2024-12-06 23:57:29 -0700

    Day 7 - Complete

--- Day07.cs
+++ Day07.cs
@@ -1,4 +1,5 @@
-﻿namespace AdventOfCode2024
+﻿
+namespace AdventOfCode2024
 {
     public class Day07 : IDay
     {
@@ -8,14 +9,85 @@
         public async Task<long> One()
         {
             var data = await Common.ReadFile(_fileDayName, "One");
-            int count = 0;
+            long count = 0;
+            var characters = new List<char> { '*', '+' };
+            foreach (var line in data)
+            {
+                long value = GetLineValue(line, characters);
+                count += value;
+            }
             return count;
+        }
+
+        private static long GetLineValue(string line, List<char> characters)
+        {
+            var parsedLine = line.Split(':');
+            var testValue = long.Parse(parsedLine[0]);
+            var numbers = parsedLine[1].Trim().Split(' ').Select(s => long.Parse(s));
+            if (LineIsValid(testValue, numbers.ToList(), characters)){
+
+                return testValue;
+            }
+            return 0;
+        }
+
+        private static bool LineIsValid(long testValue, List<long> numbers, List<char> characters)
+        {
+            if (testValue == numbers.Sum())
+            {
+                return true;
+            }else if (testValue == numbers.Aggregate(1L, (a,b) => a * b))
+            {
+                return true;
+            }
+            else
+            {
+                var variations = new Combinatorics.Collections.Variations<char>(characters, numbers.Count-1, Combinatorics.Collections.GenerateOption.WithRepetition);
+                foreach(var variation in variations)
+                {
+                    long total = numbers[0];
+
+                    for (var i = 1; i < numbers.Count; i++)
+                    {
+                        long number = numbers[i];
+                        char character = variation[i - 1];
+                        if (character == '*')
+                        {
+                            total *= number;
+                        }
+                        else if(character == '+')
+                        {
+                            total += number;
+                        }
+                        else if (character == '|')
+                        {
+                            total = long.Parse($"{total}{number}");
+                        }
+                        else
+                        {
+                            throw new NotImplementedException();
+                        }
+                    }
+                    if (total == testValue)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+
+            }
         }
 
         public async Task<long> Two()
         {
             var data = await Common.ReadFile(_fileDayName, "Two");
-            int count = 0;
+            long count = 0;
+            var characters = new List<char> { '*', '+', '|' };
+            foreach (var line in data)
+            {
+                long value = GetLineValue(line, characters);
+                count += value;
+            }
             return count;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using AdventOfCode2024;
 
-IDay dayToRun = new Day06();
+IDay dayToRun = new Day07();
 
 Console.WriteLine(dayToRun.GetName());
 Console.WriteLine($"One: {await dayToRun.One()}");
