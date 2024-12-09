534a3c6be1479cfbd8742588dcccc19f39335ef3
Dan Burgener <dan.burgener@gmail.com>
2024-12-08 23:49:52 -0700

    Day 9 - Complete

--- Day09.cs
+++ Day09.cs
@@ -45,6 +45,73 @@
             return count;
         }
 
+        public async Task<long> Two()
+        {
+            var data = await Common.ReadFile(_fileDayName, "Two");
+            var diskMap = data[0];
+            List<int?> fileData = new List<int?>();
+            List<int> fileIds = new List<int>();
+            int? currentId = 0;
+            for (var i = 0; i < diskMap.Length; i++)
+            {
+                int currentNumber = int.Parse(diskMap[i].ToString());
+                if (i % 2 == 0)
+                {
+                    fileIds.Add(currentId.Value);
+                    fileData.AddRange(Enumerable.Repeat(currentId, currentNumber));
+                }
+                else
+                {
+                    currentId++;
+                    fileData.AddRange(Enumerable.Repeat<int?>(null, currentNumber));
+                }
+            }
+            int lowestEmptyBlockIndex = 0;
+            for (var i = fileIds.Count - 1; i >= 0; i--)
+            {
+                var fileId = fileIds[i];
+                (int fileIdBlockIndex, int fileIdBlockLength) = FindNumberBlock(fileData, fileId);
+                (int nextEmptyBlockIndex, int nextEmptyBlockLength) = NextEmptyBlock(lowestEmptyBlockIndex, fileData);
+                lowestEmptyBlockIndex = nextEmptyBlockIndex;
+                while (nextEmptyBlockIndex != -1 && nextEmptyBlockIndex < fileIdBlockIndex)
+                {
+                    if (nextEmptyBlockLength >= fileIdBlockLength)
+                    {
+                        for (var j = 0; j < fileIdBlockLength; j++)
+                        {
+                            fileData.Swap(fileIdBlockIndex + j, nextEmptyBlockIndex + j);
+                        }
+                        break;
+                    }
+                    (nextEmptyBlockIndex, nextEmptyBlockLength) = NextEmptyBlock(nextEmptyBlockIndex + nextEmptyBlockLength, fileData);
+                }
+            }
+
+            long count = 0;
+            for (var i = 0; i < fileData.Count(); i++)
+            {
+                if (fileData[i].HasValue)
+                {
+                    count += (fileData[i].Value * i);
+                }
+            }
+            return count;
+        }
+
+        private (int index, int length) FindNumberBlock(List<int?> fileData, int number)
+        {
+            var index = fileData.FindIndex(i => i == number);
+            var endIndex = fileData.FindLastIndex(i => i == number);
+            return (index, endIndex - index + 1);
+        }
+
+        private (int index, int length) NextEmptyBlock(int startingIndex, List<int?> fileData)
+        {
+            var index = fileData.FindIndex(startingIndex, i => i is null);
+            var endIndex = fileData.FindIndex(index, i => i is not null);
+            return (index, endIndex - index);
+        }
+
         private int? GetLastIndexOfANumber(List<int?> fileData, int lastKnownIndexOfNumber, int minIndex)
         {
             for (var i = lastKnownIndexOfNumber; i > minIndex; i--)
@@ -55,13 +122,6 @@
                 }
             }
             return null;
-        }
-
-        public async Task<long> Two()
-        {
-            var data = await Common.ReadFile(_fileDayName, "Two");
-            int count = 0;
-            return count;
         }
     }
 }
