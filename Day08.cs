8d3a9ebf8ea9bb1abd78ddfc8fadb2d173ca35de
Dan Burgener <dan.burgener@gmail.com>
2024-12-08 21:19:05 -0700

    Switch to using Vectors

--- Day08.cs
+++ Day08.cs
@@ -6,14 +6,6 @@ namespace AdventOfCode2024
         private readonly string _fileDayName = "Eight";
         public string GetName() => "Day 08";
 
-        private enum Directions
-        {
-            DownRight,
-            DownLeft,
-            Down,
-            Right
-        }
-
         public async Task<long> One()
         {
             var data = await Common.ReadFile(_fileDayName, "One");
@@ -33,6 +25,7 @@ namespace AdventOfCode2024
             }
             return antinodes.Distinct().Count();
         }
+
         public async Task<long> Two()
         {
             var data = await Common.ReadFile(_fileDayName, "Two");
@@ -43,7 +36,7 @@ namespace AdventOfCode2024
             int colMax = data[0].Length - 1;
             foreach(var antenna in antennas) //Each antenna counts as a antinode
             {
-                antinodes.Add(GetKey(antenna.Row, antenna.Column));
+                antinodes.Add(GetKey(antenna.Vector.Y, antenna.Vector.X));
             }
             foreach (var frequency in distinctFrequencies)
             {
@@ -60,47 +53,16 @@ namespace AdventOfCode2024
         private static List<string> GetAntinodes(Antenna antennaA, Antenna antennaB, int rowMax, int colMax)
         {
             List<string> antinodes = new List<string>();
-
-            if (antennaA.Row < antennaB.Row) //A is above B
+            var newVector = antennaA.Vector - antennaB.Vector;
+            var another = antennaA.Vector + newVector;
+            if (another.X >= 0 && another.X <= colMax && another.Y >= 0 && another.Y <= rowMax)
             {
-                if (antennaA.Column < antennaB.Column) //A is left B
-                {
-                    antinodes.AddRange(GetAntinodes(antennaA, antennaB, rowMax, colMax, Directions.DownRight));
-                }
-                else if (antennaA.Column > antennaB.Column) //A is right B
-                {
-                    antinodes.AddRange(GetAntinodes(antennaA, antennaB, rowMax, colMax, Directions.DownLeft));
-                }
-                else //A and B are to the same level
-                {
-                    antinodes.AddRange(GetAntinodes(antennaA, antennaB, rowMax, colMax, Directions.Down));
-                }
+                antinodes.Add(GetKey((int)another.Y, (int)another.X));
             }
-            else if (antennaA.Row > antennaB.Row) //A is below B
+            var andAnother = antennaB.Vector - newVector;
+            if (andAnother.X >= 0 && andAnother.X <= colMax && andAnother.Y >= 0 && andAnother.Y <= rowMax)
             {
-                if (antennaA.Column < antennaB.Column) //A is left B
-                {
-                    antinodes.AddRange(GetAntinodes(antennaB, antennaA, rowMax, colMax, Directions.DownLeft));
-                }
-                else if (antennaA.Column > antennaB.Column) //A is right B
-                {
-                    antinodes.AddRange(GetAntinodes(antennaB, antennaA, rowMax, colMax, Directions.DownRight));
-                }
-                else //A and B are to the same level
-                {
-                    antinodes.AddRange(GetAntinodes(antennaB, antennaA, rowMax, colMax, Directions.Down));
-                }
-            }
-            else //A and B are to the same level
-            {
-                if (antennaA.Column < antennaB.Column) //A is left B
-                {
-                    antinodes.AddRange(GetAntinodes(antennaA, antennaB, rowMax, colMax, Directions.Right));
-                }
-                else //A is right B
-                {
-                    antinodes.AddRange(GetAntinodes(antennaB, antennaA, rowMax, colMax, Directions.Right));
-                }
+                antinodes.Add(GetKey((int)andAnother.Y, (int)andAnother.X));
             }
             return antinodes;
         }
@@ -108,211 +70,23 @@ namespace AdventOfCode2024
         private static List<string> GetAntinodesWithResonant(Antenna antennaA, Antenna antennaB, int rowMax, int colMax)
         {
             List<string> antinodes = new List<string>();
-
-            if (antennaA.Row < antennaB.Row) //A is above B
+            var newVector = antennaA.Vector - antennaB.Vector;
+            var another = antennaA.Vector + newVector;
+            while (another.X >= 0 && another.X <= colMax && another.Y >= 0 && another.Y <= rowMax)
             {
-                if (antennaA.Column < antennaB.Column) //A is left B
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaA, antennaB, rowMax, colMax, Directions.DownRight));
-                }
-                else if (antennaA.Column > antennaB.Column) //A is right B
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaA, antennaB, rowMax, colMax, Directions.DownLeft));
-                }
-                else //A and B are to the same level
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaA, antennaB, rowMax, colMax, Directions.Down));
-                }
+                antinodes.Add(GetKey((int)another.Y, (int)another.X));
+                another = another + newVector;
             }
-            else if (antennaA.Row > antennaB.Row) //A is below B
+            var andAnother = antennaB.Vector - newVector;
+            while (andAnother.X >= 0 && andAnother.X <= colMax && andAnother.Y >= 0 && andAnother.Y <= rowMax)
             {
-                if (antennaA.Column < antennaB.Column) //A is left B
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaB, antennaA, rowMax, colMax, Directions.DownLeft));
-                }
-                else if (antennaA.Column > antennaB.Column) //A is right B
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaB, antennaA, rowMax, colMax, Directions.DownRight));
-                }
-                else //A and B are to the same level
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaB, antennaA, rowMax, colMax, Directions.Down));
-                }
-            }
-            else //A and B are to the same level
-            {
-                if (antennaA.Column < antennaB.Column) //A is left B
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaA, antennaB, rowMax, colMax, Directions.Right));
-                }
-                else //A is right B
-                {
-                    antinodes.AddRange(GetAntinodesWithResonant(antennaB, antennaA, rowMax, colMax, Directions.Right));
-                }
+                antinodes.Add(GetKey((int)andAnother.Y, (int)andAnother.X));
+                andAnother = andAnother - newVector;
             }
             return antinodes;
         }
 
-        private static IEnumerable<string> GetAntinodes(Antenna antennaA, Antenna antennaB, int rowMax, int colMax, Directions direction)
-        {
-            var rowDiff = Math.Abs(antennaA.Row - antennaB.Row);
-            var colDiff = Math.Abs(antennaA.Column - antennaB.Column);
-
-            List<string> antinodes = new List<string>();
-
-            switch (direction)
-            {
-                case Directions.DownRight:
-                    {
-                        if (antennaA.Row - rowDiff >= 0 && antennaA.Column - colDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(antennaA.Row - rowDiff, antennaA.Column - colDiff));
-                        }
-                        if (antennaB.Row + rowDiff <= rowMax && antennaB.Column + colDiff <= colMax)
-                        {
-                            antinodes.Add(GetKey(antennaB.Row + rowDiff, antennaB.Column + colDiff));
-                        }
-                        break;
-                    }
-                case Directions.DownLeft:
-                    {
-                        if (antennaA.Row - rowDiff >= 0 && antennaA.Column + colDiff <= colMax)
-                        {
-                            antinodes.Add(GetKey(antennaA.Row - rowDiff, antennaA.Column + colDiff));
-                        }
-
-                        if (antennaB.Row + rowDiff <= rowMax && antennaB.Column - colDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(antennaB.Row + rowDiff, antennaB.Column - colDiff));
-                        }
-                        break;
-                    }
-                case Directions.Right:
-                    {
-                        if (antennaA.Column - colDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(antennaA.Row, antennaA.Column - colDiff));
-                        }
-
-                        if (antennaB.Column + colDiff <= colMax)
-                        {
-                            antinodes.Add(GetKey(antennaB.Row, antennaB.Column + colDiff));
-                        }
-                        break;
-                    }
-                case Directions.Down:
-                    {
-                        if (antennaA.Row - rowDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(antennaA.Row - rowDiff, antennaA.Column));
-                        }
-
-                        if (antennaB.Row + rowDiff <= rowMax)
-                        {
-                            antinodes.Add(GetKey(antennaB.Row + rowDiff, antennaB.Column));
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        throw new NotImplementedException();
-                    }
-            }
-            return antinodes;
-        }
-
-        private static IEnumerable<string> GetAntinodesWithResonant(Antenna antennaA, Antenna antennaB, int rowMax, int colMax, Directions direction)
-        {
-            var rowDiff = Math.Abs(antennaA.Row - antennaB.Row);
-            var colDiff = Math.Abs(antennaA.Column - antennaB.Column);
-
-            List<string> antinodes = new List<string>();
-
-            switch (direction)
-            {
-                case Directions.DownRight:
-                    {
-                        int currentRow = antennaA.Row;
-                        int currentCol = antennaA.Column;
-                        while(currentRow - rowDiff >= 0 && currentCol - colDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(currentRow - rowDiff, currentCol - colDiff));
-                            currentRow -= rowDiff;
-                            currentCol -= colDiff;
-                        }
-                        currentRow = antennaB.Row;
-                        currentCol = antennaB.Column;
-                        while (currentRow + rowDiff <= rowMax && currentCol + colDiff <= colMax)
-                        {
-                            antinodes.Add(GetKey(currentRow + rowDiff, currentCol + colDiff));
-                            currentRow += rowDiff;
-                            currentCol += colDiff;
-                        }
-                        break;
-                    }
-                case Directions.DownLeft:
-                    {
-                        int currentRow = antennaA.Row;
-                        int currentCol = antennaA.Column;
-                        while (currentRow - rowDiff >= 0 && currentCol + colDiff <= colMax)
-                        {
-                            antinodes.Add(GetKey(currentRow - rowDiff, currentCol + colDiff));
-                            currentRow -= rowDiff;
-                            currentCol += colDiff;
-                        }
-
-                        currentRow = antennaB.Row;
-                        currentCol = antennaB.Column;
-                        while (currentRow + rowDiff <= rowMax && currentCol - colDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(currentRow + rowDiff, currentCol - colDiff));
-                            currentRow += rowDiff;
-                            currentCol -= colDiff;
-                        }
-                        break;
-                    }
-                case Directions.Right:
-                    {
-                        int currentCol = antennaA.Column;
-                        while (currentCol - colDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(antennaA.Row, currentCol - colDiff));
-                            currentCol -= colDiff;
-                        }
-
-                        currentCol = antennaB.Column;
-                        while (currentCol + colDiff <= colMax)
-                        {
-                            antinodes.Add(GetKey(antennaB.Row, currentCol + colDiff));
-                            currentCol += colDiff;
-                        }
-                        break;
-                    }
-                case Directions.Down:
-                    {
-                        int currentRow = antennaA.Row;
-                        while (currentRow - rowDiff >= 0)
-                        {
-                            antinodes.Add(GetKey(currentRow - rowDiff, antennaA.Column));
-                            currentRow -= rowDiff;
-                        }
-
-                        currentRow = antennaB.Row;
-                        while (currentRow + rowDiff <= rowMax)
-                        {
-                            antinodes.Add(GetKey(currentRow + rowDiff, antennaB.Column));
-                            currentRow += rowDiff;
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        throw new NotImplementedException();
-                    }
-            }
-            return antinodes;
-        }
-
+        
         private List<Antenna> GetAntennas(string[] data)
         {
             List<Antenna> antennas = new List<Antenna>();
@@ -324,8 +98,7 @@ namespace AdventOfCode2024
                     {
                         antennas.Add(new Antenna
                         {
-                            Row = row,
-                            Column = column,
+                            Vector = new System.Numerics.Vector2(column, row),
                             Frequency = data[row][column]
                         });
                     }
@@ -337,11 +110,10 @@ namespace AdventOfCode2024
         public class Antenna
         {
             public char Frequency { get; set; }
-            public int Row { get; set; }
-            public int Column { get; set; }
+            public System.Numerics.Vector2 Vector { get; set; }
         }
 
-        private static string GetKey(int row, int col)
+        private static string GetKey(float row, float col)
         {
             return $"{row}-{col}";
         }
